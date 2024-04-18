0906a9915530f773e7cbff3b77f1fee66d21db84
Oludire Damilare <oludire@gmail.com>
2024-04-18 20:27:50 +0100

    Footer completed

--- src/App.js
+++ src/App.js
@@ -1,82 +1,47 @@
-import { ReactComponent as Logo } from './images/logo.svg';
+import logo from './images/logo.svg';
+import logoFooter from './images/logo-footer.svg';
 
-import online from './images/icon-online.svg';
-import budgeting from './images/icon-budgeting.svg';
-import onboarding from './images/icon-onboarding.svg';
-import api from './images/icon-api.svg';
+import { easyBank, latestArticles } from './easyBank';
 
-const latestArticles = [
-  {
-    image: './images/image-currency.jpg',
-    alt: 'currency',
-    author: 'Claire Robinson',
-    title: 'Receive money in any currency with no fees',
-    article:
-      'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …',
-  },
-  {
-    image: './images/image-restaurant.jpg',
-    alt: 'restaurant',
-    author: 'Wilson Hutton',
-    title: 'Treat yourself without worrying about money',
-    article:
-      'Our simple budgeting feature allows you to separate out your spending and setrealistic limits each month. That means you …',
-  },
-  {
-    image: './images/image-plane.jpg',
-    author: 'Wilson Hutton',
-    title: 'Take your Easybank card wherever you go',
-    article:
-      'We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …',
-  },
-  {
-    image: './images/image-confetti.jpg',
-    author: 'Claire Robinson',
-    title: 'Our invite-only Beta accounts are now live!',
-    article:
-      'After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...',
-  },
-];
-
-const easyBank = [
-  {
-    image: online,
-    title: 'Online Banking',
-    text: 'Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.',
-  },
-  {
-    image: budgeting,
-    title: 'Simple Budgeting',
-    text: 'See exactly where your money goes each month. Receive notifications when you’re close to hitting your limits.',
-  },
-  {
-    image: onboarding,
-    title: 'Fast Onboarding',
-    text: 'We don’t do branches. Open your account in minutes online and start taking control of your finances right away.',
-  },
-  {
-    image: api,
-    title: 'Open API',
-    text: 'Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.',
-  },
-];
+import facebook from './images/icon-facebook.svg';
+import youtube from './images/icon-youtube.svg';
+import twitter from './images/icon-twitter.svg';
+import pinterest from './images/icon-pinterest.svg';
+import instagram from './images/icon-instagram.svg';
 
 export default function App() {
   return (
     <div className="font-Sans text-center">
-      <Header />
-      <NextGen />
-      <EasyBank />
-      <Articles />
-      <Footer />
+      <EasyBankApp />
     </div>
   );
 }
 
-function Header() {
+function EasyBankApp() {
+  return (
+    <div>
+      <Header logo={logo} />
+      <NextGen />
+      <EasyBank />
+      <Articles />
+      <Footer
+        logo={logoFooter}
+        facebook={facebook}
+        twitter={twitter}
+        youtube={youtube}
+        pinterest={pinterest}
+        instagram={instagram}
+      />
+    </div>
+  );
+}
+
+function Header({ logo }) {
   return (
     <header className="w-full flex flex-row bg-white justify-between py-3 px-5 fixed z-10">
-      <Logo />
+      <div className="">
+        <img src={logo} alt="logo" />
+      </div>
       <img src="./images/icon-hamburger.svg" alt="menu" className="w-10 h-4 cursor-pointer" />
     </header>
   );
@@ -89,10 +54,10 @@ function NextGen() {
       <img
         src="./images/image-mockups.png"
         alt="mockupImage"
-        className="h-[38rem] m-auto absolute top-[-140px] p-5 w-auto"
+        className="sm:h-[30rem] m-auto absolute top-[-100px] p-5 w-auto"
       />
 
-      <h1 className="text-[3rem] text-blue font-light leading-normal w-auto mx-auto">
+      <h1 className="text-[2.5rem] text-blue font-light leading-normal w-auto mx-auto">
         Next generation digital banking
       </h1>
       <p className="text-gray-300 py-[2rem] w-auto mx-auto px-12 text-base text-justify">
@@ -161,17 +126,84 @@ function Articles() {
 
 function Article({ image, author, title, article, alt }) {
   return (
-    <div className="bg-gray-200 flex flex-col items-center px-5">
+    <div className="bg-gray-200 flex flex-col items-center px-5 cursor-pointer">
       <img src={image} alt={alt} className="w-auto mx-auto rounded-t-[0.5rem]" />
       <div className="bg-white text-start p-10">
         <p className="font-light text-[0.25rem] text-gray-300">By {author}</p>
-        <h2 className="text-blue text-base">{title}</h2>
+        <h2
+          className="text-blue text-base
+         hover:text-green"
+        >
+          {title}
+        </h2>
         <p className="font-light text-[0.7rem] text-gray-300">{article}</p>
       </div>
     </div>
   );
 }
 
-function Footer() {
-  return <footer></footer>;
+function Footer({ logo, facebook, twitter, youtube, pinterest, instagram }) {
+  return (
+    <footer className="bg-blue text-white  flex flex-col justify-center items-center py-10">
+      <div className="py-7">
+        <img src={logo} alt="logo" />
+      </div>
+      <div>
+        <ul className="flex  space-x-5">
+          <li>
+            <a href="#facebook">
+              <img src={facebook} alt="facebook" />
+            </a>
+          </li>
+          <li>
+            <a href="#youtube">
+              <img src={youtube} alt="youtube" />
+            </a>
+          </li>
+          <li>
+            <a href="#twitter">
+              <img src={twitter} alt="twitter" />
+            </a>
+          </li>
+          <li>
+            <a href="#pinterest">
+              <img src={pinterest} alt="pinterest" />
+            </a>
+          </li>
+          <li>
+            <a href="#instagram">
+              <img src={instagram} alt="instagram" />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul className="py-5">
+          <li className="py-2 hover:text-green">
+            <a href="#aboutUs">About Us</a>
+          </li>
+          <li className="py-2 hover:text-green">
+            <a href="#contact">Contact</a>
+          </li>
+          <li className="py-2 hover:text-green">
+            <a href="#blog">Blog</a>
+          </li>
+          <li className="py-2 hover:text-green">
+            <a href="#careers">Careers</a>
+          </li>
+          <li className="py-2 hover:text-green">
+            <a href="#support">Support</a>
+          </li>
+          <li className="py-2 hover:text-green">
+            <a href="#privacypolicy">Privacy Policy</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="bg-gradient-to-r from-green to-cyan  px-9 py-3 my-5 rounded-full cursor-pointer ">
+        Request Invite
+      </div>
+      <p className="text-gray-300">&copy; EasyBank. All Rights Reserved</p>
+    </footer>
+  );
 }
